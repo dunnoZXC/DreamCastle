0a4a98c964e998f1a0ccace9cc0784e82876ff67
dunnoZXC <mishkamayevskiy007@gmail.com>
2026-06-18 21:55:21 +0300

    Deploy DreamCastle to GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
